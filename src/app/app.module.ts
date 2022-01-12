de722e9e14944171c2247b54302ae67bed39a755
jcoble <coble.jesse@gmail.com>
2022-01-12 05:45:36 -0500

    initial with material and tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider'
+import { MatButtonModule } from '@angular/material/button'
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+MatSliderModule,
+MatButtonModule
+,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
